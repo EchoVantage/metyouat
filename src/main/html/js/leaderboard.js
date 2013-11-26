9974efb99746bc25ca61e110c7e92c80e51259fc
Mike Deck <mike.deck@echovantage.com>
2013-11-26 10:15:48 -0600

    aesthetic improvements to leaderboard

--- src/main/html/js/leaderboard.js
+++ src/main/html/js/leaderboard.js
@@ -10,6 +10,42 @@ function MyaStream($scope) {
 			 content: '#IMet @mikedeck today'},
 			{avatar: 'https://pbs.twimg.com/profile_images/378800000506746994/dcef7ed5fbe93860c74b892311f74aa4_normal.jpeg',
 			 sender: 'Sarah Kayfus',
+			 content: '@fuwjax good to meet you at the Thanksgiving party #CommonDesk #IMet'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000723461847/0b527619cc7ed2c0fc74524cc5771b53_normal.jpeg',
+			 sender: 'Jeremy Krakowski',
+			 content: '#IMet @sarahkayfus #hottie #flexin #smellsgood'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000241505326/984d58129e98651947cca7ce38bc409b_normal.jpeg',
+			 sender: 'Dennis Smolek',
+			 content: '#IMet @mikedeck today'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000506746994/dcef7ed5fbe93860c74b892311f74aa4_normal.jpeg',
+			 sender: 'Sarah Kayfus',
+			 content: '@fuwjax good to meet you at the Thanksgiving party #CommonDesk #IMet'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000723461847/0b527619cc7ed2c0fc74524cc5771b53_normal.jpeg',
+			 sender: 'Jeremy Krakowski',
+			 content: '#IMet @sarahkayfus #hottie #flexin #smellsgood'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000241505326/984d58129e98651947cca7ce38bc409b_normal.jpeg',
+			 sender: 'Dennis Smolek',
+			 content: '#IMet @mikedeck today'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000506746994/dcef7ed5fbe93860c74b892311f74aa4_normal.jpeg',
+			 sender: 'Sarah Kayfus',
+			 content: '@fuwjax good to meet you at the Thanksgiving party #CommonDesk #IMet'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000723461847/0b527619cc7ed2c0fc74524cc5771b53_normal.jpeg',
+			 sender: 'Jeremy Krakowski',
+			 content: '#IMet @sarahkayfus #hottie #flexin #smellsgood'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000241505326/984d58129e98651947cca7ce38bc409b_normal.jpeg',
+			 sender: 'Dennis Smolek',
+			 content: '#IMet @mikedeck today'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000506746994/dcef7ed5fbe93860c74b892311f74aa4_normal.jpeg',
+			 sender: 'Sarah Kayfus',
+			 content: '@fuwjax good to meet you at the Thanksgiving party #CommonDesk #IMet'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000723461847/0b527619cc7ed2c0fc74524cc5771b53_normal.jpeg',
+			 sender: 'Jeremy Krakowski',
+			 content: '#IMet @sarahkayfus #hottie #flexin #smellsgood'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000241505326/984d58129e98651947cca7ce38bc409b_normal.jpeg',
+			 sender: 'Dennis Smolek',
+			 content: '#IMet @mikedeck today'},
+			{avatar: 'https://pbs.twimg.com/profile_images/378800000506746994/dcef7ed5fbe93860c74b892311f74aa4_normal.jpeg',
+			 sender: 'Sarah Kayfus',
 			 content: '@fuwjax good to meet you at the Thanksgiving party #CommonDesk #IMet'}
 		];
 }
@@ -43,6 +79,24 @@ function MyaLeaderboard($scope) {
 		{
 			avatar: 'https://pbs.twimg.com/profile_images/3318020339/baf83ef0f75702c87bc9b7a73938e886_bigger.jpeg',
 			name: 'Elizabeth Adams',
+			points: 29,
+			connections: 12
+		},
+		{
+			avatar: 'https://pbs.twimg.com/profile_images/3114998047/312221bc500181c10a6da9989be04530_bigger.jpeg',
+			name: 'Aaron King',
+			points: 28,
+			connections: 11
+		},
+		{
+			avatar: 'https://pbs.twimg.com/profile_images/378800000723461847/0b527619cc7ed2c0fc74524cc5771b53_bigger.jpeg',
+			name: 'Jeremy Krakowski',
+			points: 10,
+			connections: 1
+		},
+		{
+			avatar: 'https://pbs.twimg.com/profile_images/378800000244234387/bac76d8acf41e69d3bca999380481d9e_bigger.png',
+			name: 'Arturo Torres',
 			points: 29,
 			connections: 12
 		},
